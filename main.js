3638da37168e487eb93fce76934515a0e055301a
Malyshev R <malyshev.dev@ya.ru>
2021-12-15 15:31:29 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 // main.js
 
-// Modules to control application life and create native browser window
 //System modules
 const { BrowserWindow, Menu, app } = require('electron')
 const path = require('path')
@@ -52,37 +51,24 @@ async function createWindow () {
     });
         //}
 
-    //mainWindow.loadURL('https://google.com/')
-    // and load the index.html of the app.
-    //mainWindow.loadFile('index.html')
-
     // Open the DevTools.
     // mainWindow.webContents.openDevTools()
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
+
 // Некоторые интерфейсы API могут использоваться только после возникновения этого события.
 //app.commandLine.appendSwitch('remote-debugging-port', '3000')
 //app.commandLine.appendSwitch('host-rules', 'MAP *.google.com proxy')
-
 
 app.whenReady().then(() => {
     createWindow()
     //app.commandLine.appendSwitch('proxy-bypass-list', '<local>;*.google.com;*foo.com;1.2.3.4:5678')
     app.on('activate', function () {
-        // On macOS it's common to re-create a window in the app when the
-        // dock icon is clicked and there are no other windows open.
         if (BrowserWindow.getAllWindows().length === 0) createWindow()
     })
 })
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
 app.on('window-all-closed', function () {
     if (process.platform !== 'darwin') app.quit()
 })
 
-// In this file you can include the rest of your app's specific main process
-// code. Можно также поместить их в отдельные файлы и применить к ним require.
